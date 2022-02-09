0a1e26b4aae1b64d7b89ac3c17608a0ba4ff487c
rjsanjaymandal <rjsanjaymandal@gmail.com>
2022-02-09 21:50:49 +0530

    1.2

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+
+<?php
+$insert= false;
+if(isset($_POST['name'])){
+$server ="localhost";
+$username ="root";
+$password ="";
+$con = mysqli_connect($server, $username, $password);
+if(!$con){
+    die("error" . mysqli_connect_error());
+}
+   $name = $_POST['name'];
+   $age = $_POST['age'];
+   $gender = $_POST['gender'];
+   $email = $_POST['email'];
+   $phone = $_POST['phone'];
+   $desc = $_POST['desc'];
+   $sql="INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());" ;
+
+if($con->query($sql) == true){
+       $insert = true;
+}
+else{
+    echo "ERROR: $sql <br> $con->error";
+}
+$con->close();
+}
+?>
+<!-- *****************************************html*********************************************************** -->
+<!-- ******************************************************************************************************** -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +41,13 @@
     <div class="container">
         <h1>welcome to IIT travel form</h1>
         <P>enter your information and submit your to conform your participation in the trip</P>
-        <p class="submitMsg">thanks for submitting</p>
+        <?php
+        if($insert==true){
+        echo "
+        <p class='submitMsg'>thanks for submitting
+        </p>";
+        }
+        ?>
     <form action="index.php" method="post">
         <input type="text" name="name" id="name" placeholder="enter your name">
         <input type="text" name="age" id="age" placeholder="enter your age">
